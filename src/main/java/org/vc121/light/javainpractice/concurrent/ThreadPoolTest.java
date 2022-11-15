340ff9bc5bc4b19315e590ab7a44a44aa8cfb442
Sam Lu <luxiaocong524@163.com>
2022-11-15 10:22:57 +0800

    feat: 修改 ThreadPoolTest

--- src/main/java/org/vc121/light/javainpractice/concurrent/ThreadPoolTest.java
+++ src/main/java/org/vc121/light/javainpractice/concurrent/ThreadPoolTest.java
@@ -10,10 +10,40 @@ import java.util.concurrent.Executors;
 public class ThreadPoolTest {
 
     public static void main(String[] args) {
-        ExecutorService executorService = Executors.newSingleThreadExecutor();
-        executorService.execute(() -> System.out.println(Thread.currentThread().getName() + ": AAA"));
-        executorService.execute(() -> System.out.println(Thread.currentThread().getName() + ": BBB"));
-        executorService.shutdown();
+        ExecutorService singleThreadExecutor = Executors.newSingleThreadExecutor();
+        singleThreadExecutor.execute(() -> System.out.println(Thread.currentThread().getName() + ": AAA"));
+        singleThreadExecutor.execute(() -> System.out.println(Thread.currentThread().getName() + ": BBB"));
+        singleThreadExecutor.execute(() -> System.out.println(Thread.currentThread().getName() + ": CCC"));
+        singleThreadExecutor.execute(() -> System.out.println(Thread.currentThread().getName() + ": DDD"));
+        singleThreadExecutor.shutdown();
+
+        ExecutorService fixedThreadPool = Executors.newFixedThreadPool(3);
+        fixedThreadPool.execute(() -> System.out.println(Thread.currentThread().getName() + ": AAA"));
+        fixedThreadPool.execute(() -> System.out.println(Thread.currentThread().getName() + ": BBB"));
+        fixedThreadPool.execute(() -> System.out.println(Thread.currentThread().getName() + ": CCC"));
+        fixedThreadPool.execute(() -> System.out.println(Thread.currentThread().getName() + ": DDD"));
+        fixedThreadPool.shutdown();
+
+        ExecutorService cachedThreadPool = Executors.newCachedThreadPool();
+        cachedThreadPool.execute(() -> System.out.println(Thread.currentThread().getName() + ": AAA"));
+        cachedThreadPool.execute(() -> System.out.println(Thread.currentThread().getName() + ": BBB"));
+        cachedThreadPool.execute(() -> System.out.println(Thread.currentThread().getName() + ": CCC"));
+        cachedThreadPool.execute(() -> System.out.println(Thread.currentThread().getName() + ": DDD"));
+        cachedThreadPool.shutdown();
+
+        ExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(3);
+        scheduledExecutorService.execute(() -> System.out.println(Thread.currentThread().getName() + ": AAA"));
+        scheduledExecutorService.execute(() -> System.out.println(Thread.currentThread().getName() + ": BBB"));
+        scheduledExecutorService.execute(() -> System.out.println(Thread.currentThread().getName() + ": CCC"));
+        scheduledExecutorService.execute(() -> System.out.println(Thread.currentThread().getName() + ": DDD"));
+        scheduledExecutorService.shutdown();
+
+        ExecutorService workStealingPool = Executors.newWorkStealingPool(3);
+        workStealingPool.execute(() -> System.out.println(Thread.currentThread().getName() + ": AAA"));
+        workStealingPool.execute(() -> System.out.println(Thread.currentThread().getName() + ": BBB"));
+        workStealingPool.execute(() -> System.out.println(Thread.currentThread().getName() + ": CCC"));
+        workStealingPool.execute(() -> System.out.println(Thread.currentThread().getName() + ": DDD"));
+        workStealingPool.shutdown();
     }
 
 }
